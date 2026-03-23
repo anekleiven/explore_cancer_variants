e4a4d549d5c9241af4a1da718f08ca37d68dbf8e
anekleiven <ane.kleiven@gmail.com>
2026-03-23 08:55:59 +0100

    update gnomAD, add new folder

--- scripts/EDA/gnomAD_freq.py
+++ scripts/EDA/gnomAD_freq.py
@@ -1,20 +1,23 @@
-"""
-ANALYSIS: gnomAD allele frequencies
-----------------------------------------
+# ===============================================================
+# EDA: gnomAD Allele Frequencies
+# ===============================================================
 
+"""
 Script: gnomAD_freq.py
 Author: Ane Kleiven
 
-This script performs a multi-step analysis to explore how somatic cancer
-variants distribute across gnomAD allele frequencies. 
+This script performs a multi-step explorative data analysis on 
+gnomAD allele frequencies. 
 
 Script content:
 --------------
-1. Function to analyze and plot gnomAD allele frequencies for a given oncogenicity class. 
-2. gnomAD AF analysis for all oncogenicity classes 
-3. Log-scaled KDE-comparison between oncogenic and likely neutral variants. 
-4. Descriptive statistics 
- 
+1. Load variant data 
+2. Filter and clean variant data 
+3. Perform descriptive statistics per oncogenicity class 
+4. gnomAD AF analysis per oncogenicity class
+5. Log-scaled KDE-comparison between oncogenic and likely neutral variants. 
+
+
 All plots are saved in:
     plots/
 
@@ -30,7 +33,30 @@ print("-"*50)
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
+import argparse
+from pathlib import Path
+
+# -------------------------------------------
+# Argparse function for user input file paths
+# -------------------------------------------
+
+def getargs(): 
+    parser = argparse.ArgumentParser(
+        description="Explore gnomAD AF in variant data."
+    ) 
+
+    parser.add_argument(
+        "--variants", 
+        type=Path, 
+        required=False, 
+        default="/home/anekl/git/master/cancer_variants_annotation_pipeline/output/variants_with_maves.tsv",
+        help="Path to the input file with variant data."
+    )
+
+    return parser.parse_args() 
+
+
+args = getargs() 
 
 # ------------------------------------------------------------
 # Load variant data
@@ -39,41 +65,70 @@ import numpy as np
 print("Loading variant data..")
 
 variants = pd.read_csv(
-    "/home/anekl/git/master/cancer_variants_annotation_pipeline/output/variants_with_maves.tsv",
+    args.variants,
     sep="\t",
     low_memory=False
 )
 
-print(f"Loaded {len(variants):,} variants.")
+print(f"Loaded {len(variants):,} variants.\n")
 
+# ------------------------------------------------------------
+# Filter and clean data 
+# ------------------------------------------------------------
 
-# ============================================================
+print("Filter and clean gnomAD data..")
+# Select classes 
+wanted = ["Oncogenic", "Likely Neutral"]
+filtered = variants[variants["ONCOGENIC"].isin(wanted)].copy()
+
+# Convert gnomAD_AF to numeric
+filtered["gnomAD_AF"] = pd.to_numeric(filtered["gnomAD_AF"], errors="coerce")
+
+# Drop NA and zero values
+filtered = filtered[filtered["gnomAD_AF"].notna() & (filtered["gnomAD_AF"] > 0)]
+
+print(f"Variants after filtering: {len(filtered)}")
+print(f"Variants with missing gnomAD: {len(variants) - len(filtered)}")
+print(f"{len(filtered)/len(variants) * 100:.2f}% variants have gnomAD allele frequencies.")
+print("-"*50)
+
+# ------------------------------------------------------------
+# Descriptive statistics per oncogenicity class
+# ------------------------------------------------------------
+
+# Group by oncogenicity and calculate descriptive statistics 
+stats_summary = filtered.groupby("ONCOGENIC")["gnomAD_AF"].agg(
+    count='count',
+    median='median',
+    mean='mean',
+    std='std'
+).reset_index()
+
+print("Decriptive statistics by oncogenicity:")
+print(stats_summary)
+print("-"*50)
+
+# ------------------------------------------------------------
 # Function to analyze gnomAD allele frequencies
-# ============================================================
+# ------------------------------------------------------------
 
-def analyze_gnomad_af(variants: pd.DataFrame, status: str, plotname: str, color: str = "teal"):
-    """Analyze and plot gnomAD_AF distribution for a given ONCOGENIC category."""
+def analyze_gnomad_af(df: pd.DataFrame, status: str, plotname: str, color: str = "teal"):
+    """
+    Analyze and plot gnomAD_AF for a given oncogencity class.
+    Log-scaled distribution. 
 
+    """
     print(f"Extracting gnomAD allele frequencies for variants with '{status}' oncogenicity..")
 
-    subset = variants[variants["ONCOGENIC"] == status].copy()
-    total = len(subset)
-    print(f"Found {total:,} variants with '{status}' oncogenicity.\n")
-
-    # Convert to numeric and separate valid frequencies
-    subset["gnomAD_AF"] = pd.to_numeric(subset["gnomAD_AF"], errors="coerce")
-    subset_af = subset[subset["gnomAD_AF"].notna() & subset["gnomAD_AF"] > 0]
-    missing_af = subset["gnomAD_AF"].isna().sum()
-
-    print(f"{missing_af:,} of {total:,} '{status}' variants "
-          f"({100 * missing_af / total:.1f}%) lack gnomAD allele frequency data.")
+    subset = df[df["ONCOGENIC"] == status].copy()
+    print(f"Found {len(subset):,} variants with '{status}' oncogenicity.")
 
     # Plot distribution
-    print("\nPlotting gnomAD allele frequency distribution (log-scaled)..\n")
+    print("Plotting gnomAD allele frequency distribution (log-scaled)..\n")
     plt.figure(figsize=(8,5))
 
     sns.histplot(
-        data=subset_af,
+        data=subset,
         x="gnomAD_AF",
         log_scale=True,
         bins=50,
@@ -90,71 +145,50 @@ def analyze_gnomad_af(variants: pd.DataFrame, status: str, plotname: str, color:
     plt.legend(loc='upper right')
 
     plt.tight_layout()
-    plt.savefig(f"plots/{plotname}", dpi=300, bbox_inches="tight")
+    plt.savefig(f"plots/gnomad/{plotname}", dpi=300, bbox_inches="tight")
     plt.show()
   
     # Count rare vs common
-    common = (subset_af["gnomAD_AF"] > 0.01).sum()
-    rare = (subset_af["gnomAD_AF"] <= 0.01).sum()
+    common = (subset["gnomAD_AF"] > 0.01).sum()
+    rare = (subset["gnomAD_AF"] <= 0.01).sum()
 
     print("\nDistribution of common vs. rare variants:")
-    print(f"Common '{status}' variants (gnomAD_AF > 0.01): {common:,}")
-    print(f"Rare   '{status}' variants (gnomAD_AF ≤ 0.01): {rare:,}")
-    print(f"Total with AF available: {len(subset_af):,}")
-    print("-"*30)
+    print(f"Common '{status}' variants (gnomAD_AF > 0.001): {common:,}")
+    print(f"Rare   '{status}' variants (gnomAD_AF ≤ 0.001): {rare:,}")
+    print(f"Total with AF available: {len(subset):,}")
+    print("-"*50)
 
-# ============================================================
-# Run analysis for all oncogenic categories 
-# ============================================================
+# ------------------------------------------------------------
+# Run analysis for all oncogenic classes
+# ------------------------------------------------------------
 
-print("-"*30)
 print("Running gnomAD AF analysis for all given oncogenicity classes..\n")
 
-# 1. Unknown
-analyze_gnomad_af(variants, "Unknown", color="#848a8e", plotname="gnomAD_unknown.png")
+# Oncogenic
+analyze_gnomad_af(filtered, "Oncogenic", color="#c4314a", plotname="gnomAD_onco.png")
 
-# 2. Likely Oncogenic
-analyze_gnomad_af(variants, "Likely Oncogenic", color="#D98C6A", plotname="gnomAD_likely_onco.png")
+# Likely Neutral
+analyze_gnomad_af(variants, "Likely Neutral", color="#88aed1", plotname="gnomAD_likely_neutral.png")
 
-# 3. Oncogenic
-analyze_gnomad_af(variants, "Oncogenic", color="#C4473B", plotname="gnomAD_onco.png")
-
-# 4. Inconclusive
-analyze_gnomad_af(variants, "Inconclusive", color="#f9c74f", plotname="gnomAD_inconclusive.png")
-
-# 5. Likely Neutral
-analyze_gnomad_af(variants, "Likely Neutral", color="#7e8aa2", plotname="gnomAD_likely_neutral.png")
-
-print("gnomAD frequency analysis completed successfully for all oncogenicity classes.")
-print("Plots saved in folder 'plots/'")
+print("gnomAD frequency analysis successfully completed for all oncogenicity classes.")
+print("Plots saved in folder 'plots/gnomad/'")
 
 
-# ============================================================
+# ------------------------------------------------------------
 # Log-scaled KDE comparison 
-# ============================================================
+# ------------------------------------------------------------
 
-print("-"*30)
-print("Log-scaled KDE comparison plot (oncogenic vs likely neutral)")
+print("-"*50)
+print("Log-scaled KDE comparison plot (Oncogenic vs. Likely Neutral)")
 
-
-# select classes 
-wanted = ["Oncogenic", "Likely Neutral"]
-filtered = variants[variants["ONCOGENIC"].isin(wanted)].copy()
-
-# convert gnomAD_AF to numeric
-filtered["gnomAD_AF"] = pd.to_numeric(filtered["gnomAD_AF"], errors="coerce")
-
-# drop NA and zero values
-filtered = filtered[filtered["gnomAD_AF"].notna() & (filtered["gnomAD_AF"] > 0)]
-
-# define colors for the given classes
+# Define colors for the given classes
 palette = {
-    "Oncogenic": "#C4473B",
-    "Likely Neutral": "#7e8aa2",
+    "Oncogenic": "#c4314a",
+    "Likely Neutral": "#88aed1",
     }
 
-# create log-scaled density plot 
-print("Plotting density of gnomAD allele frequencies for oncogenic vs likely neutral..\n")
+# Create log-scaled density plot 
+print("Plotting density of gnomAD allele frequencies for Oncogenic vs. Likely Neutral..\n")
 
 plt.figure(figsize=(8,5)) 
 
@@ -171,26 +205,13 @@ sns.kdeplot(
 plt.title("gnomAD AF Distribution Across Oncogenicity Classes", fontsize=14, pad=10) 
 plt.xlabel("gnomAD_AF (log10 scale)", fontsize=12)
 plt.ylabel("Density", fontsize=12)
+
 plt.tight_layout()
-
-plt.savefig("plots/gnomAD_combined_KDE.png", dpi=300, bbox_inches="tight") 
-
+plt.savefig("plots/gnomad/gnomAD_combined_KDE.png", dpi=300, bbox_inches="tight") 
 plt.show() 
 
-print("Plotting complete! Plot saved as 'plots/gnomAD_combined_KDE.png'\n")
-
-
-# Group by oncogenicity and calculate descriptive statistics 
-stats_summary = filtered.groupby("ONCOGENIC")["gnomAD_AF"].agg(
-    count='count',
-    median='median',
-    mean='mean',
-    std='std'
-).reset_index()
-
-print("Decriptive statistics by oncogenicity:")
-print(stats_summary,"\n")
+print("Plotting complete! Plot saved as 'plots/gnomad/gnomAD_combined_KDE.png'")
+print("-"*50)
 
 
 print("\nVariant gnomAD allele frequency analysis complete!🥳🥳\n")
-
